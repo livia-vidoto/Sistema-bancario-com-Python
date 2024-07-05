9c46253de10b40a5d48446343141ab2ff683d891
livia-vidoto <vidotolivia7@gmail.com>
2024-07-04 21:02:25 -0300

    Update otimizando-o-sistema-bancario-utilizando-funcoes-python.py

--- otimizando-o-sistema-bancario-utilizando-funcoes-python.py
+++ otimizando-o-sistema-bancario-utilizando-funcoes-python.py
@@ -1,56 +1,63 @@
 import textwrap
 
+
 def menu():
-  menu = """/n
-  Seja Bem-Vindo ao PyPag!
-  ============= MENU =============
-  [1]\tDepositar
-  [2]\tSacar
-  [3]\tExtrato
-  [4]\tNova conta
-  [5]\tlistar contas
-  [6]\tNovo usuário
-  [x]\tSair
-  => """
-  return input(textwrap.dedent(menu))
+    menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
+
 
 def depositar(saldo, valor, extrato, /):
-  if saldo >= 0:
-    saldo += valor
-    extrato += f'Depósito:\t R$ {valor:.2f}\n'
-    print('\n===== Operação realizada com sucesso! =====')
-    
-  else:
-    ('\n===== Operação inválida, tente novamente! ====')
-  return saldo, extrato
-
-def sacar (*, saldo, valor, extrato, limite, numero_saques, limite_saques):
-    excedeu_saldo = valor > saldo
-    excedeu_limite = valor > limite
-    excedeu_saques = numero_saques >= limite_saques
-  if excedeu_saldo:
-    print('\n==== Operação falhou! Você não possui saldo suficiente para realiizar esta operação. ====')
-    
-  elif excedeu_limite:
-    print('\n==== Operação falhou! O valor do saque é maior que o limite. ====')
-  
-  elif valor > 0:
-        saldo -= valor
-        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
-        numero_saques += 1
-        print("\n==== Saque realizado com sucesso! ====")
-  else:
-    print('\n==== Operação falhou! Valor informado inválido. ====')
-     else:
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
         print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
     return saldo, extrato
+
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
+
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+    return saldo, extrato
+
 
 def exibir_extrato(saldo, /, *, extrato):
     print("\n================ EXTRATO ================")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
     print("==========================================")
+
 
 def criar_usuario(usuarios):
     cpf = input("Informe o CPF (somente número): ")
@@ -151,5 +158,3 @@ def main():
 
 
 main()
-
-    
